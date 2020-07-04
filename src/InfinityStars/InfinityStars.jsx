569c808de9f774dceea0ebf8fd36e58b3a85e6de
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-03 23:36:43 -0400

    yay

---
+++ src/InfinityStars/InfinityStars.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import * as THREE from "three";
+
+class InfinityStars extends Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <>
+        <div className="text-box">
+          <div class="heading">To infinity and Beyond</div>
+          <div class="button-wrapper"></div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default InfinityStars;
