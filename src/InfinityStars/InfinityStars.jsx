fc71baf936e87bc01a471299a17bcbfa21ce1905
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-04 11:18:36 -0400

    yay

--- src/InfinityStars/InfinityStars.jsx
+++ src/InfinityStars/InfinityStars.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import * as THREE from "three";
 import STARLITE from "../InfinityStars/star.png";
+import "./InfinityStars.css";
 
 let scene, camera, renderer, stars, starGeo;
 
@@ -16,13 +17,9 @@ class InfinityStars extends Component {
       );
       camera.position.z = 1;
       camera.position.x = Math.PI / 2;
-
       renderer = new THREE.WebGLRenderer();
       renderer.setSize(window.innerWidth, window.innerHeight);
-
       document.body.appendChild(renderer.domElement);
-      // this.mount.appendChild(renderer.domElement);
-
       starGeo = new THREE.Geometry();
       for (let i = 0; i < 6000; i++) {
         let star = new THREE.Vector3(
@@ -34,33 +31,26 @@ class InfinityStars extends Component {
         star.acceleration = 0.02;
         starGeo.vertices.push(star);
       }
-
       let sprite = new THREE.TextureLoader().load(STARLITE);
       let starMaterial = new THREE.PointsMaterial({
         color: 0xaaaaaa,
         size: 0.7,
         map: sprite,
       });
-
       stars = new THREE.Points(starGeo, starMaterial);
       scene.add(stars);
-
       window.addEventListener("resize", onWindowResize, false);
-
       animate();
     }
-
     function onWindowResize() {
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
     }
-
     function animate() {
       starGeo.vertices.forEach((p) => {
         p.velocity += p.acceleration;
         p.y -= p.velocity;
-
         if (p.y < -200) {
           p.y = 200;
           p.velocity = 0;
@@ -77,7 +67,11 @@ class InfinityStars extends Component {
   render() {
     return (
       <>
-      <div ref={(ref) => (this.mount = ref)} />
+        <div ref={(ref) => (this.mount = ref)}>
+          <div className="text-box">
+            <div className="heading">To Infinity And Beyond</div>
+          </div>
+        </div>
       </>
     );
   }
