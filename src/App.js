4a80d4206929f82b336ec6bf800ae8aa38792d8e
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-20 23:30:25 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import InfinityStars from "./InfinityStars/InfinityStars";
+import Zoom from "react-reveal/Zoom";
 // import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div>
+      <Zoom delay={2000} duration={4000}>
         <InfinityStars />
-      </div>
+      </Zoom>
     </>
   );
 }
--- src/InfinityStars/InfinityStars.jsx
+++ src/InfinityStars/InfinityStars.jsx
@@ -71,8 +71,9 @@ class InfinityStars extends Component {
       <>
         <div ref={(ref) => (this.mount = ref)}>
           <div className="text-box">
-            <div className="heading">To Infinity And Beyond</div>
-            <div className="heading">&nbsp;&nbsp;Thanks To Three.JS</div>
+            <div className="heading">Infinite-Stars</div>
+            <br />
+            <div className="heading">&nbsp;&nbsp;&nbsp;Three.JS</div>
           </div>
         </div>
       </>
