7b3c29d5203d8a4065dad15d61df447b8e31e837
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-05 15:28:53 -0400

    Updates

---
+++ precache-manifest.0a881a7cef5c3a6698f596a705d5072c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352c162d20bbd3a15667a191d938913f",
+    "url": "/InfinityStars/index.html"
+  },
+  {
+    "revision": "545f754c50d72dc1c71e",
+    "url": "/InfinityStars/static/css/main.4c37db02.chunk.css"
+  },
+  {
+    "revision": "57c098f241a45fce0d09",
+    "url": "/InfinityStars/static/js/2.609ad3a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/InfinityStars/static/js/2.609ad3a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545f754c50d72dc1c71e",
+    "url": "/InfinityStars/static/js/main.c542c086.chunk.js"
+  },
+  {
+    "revision": "32cd16886989f0401c71",
+    "url": "/InfinityStars/static/js/runtime-main.01268236.js"
+  }
+]);
